79260aa159e37a1165620f4fea749f763910a025
nicollas231344 <nicollas.franco@escola.pr.gov.br>
2025-06-26 08:43:01 -0300

    nmf

---
+++ script.js
@@ -0,0 +1,12 @@
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button =>{
+    button.addEventListener('click',function(){
+        const atual = document.querySelector('.ativo')
+        const proximoPasso = 'passo-' + this.getAttribut('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById('prximoPasso'). classList.add('ativo')
+
+    })
+})
